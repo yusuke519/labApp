fd549771f91bfcbe8863cdef7bd4fd47743cd297
yu_suke519 <sabumarin.dearisu@gmail.com>
2013-10-02 08:21:03 +0900

    heroku application(testApp)

---
+++ rails/railsTest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTest::Application.config.secret_key_base = '1880fe3c76cf060a5e3e6728a31213bfdbc320e2a6fb39d7509cd8d3486d97f4e05a0951bf5afc2172244017adbd42968dee316fdd9ebed63047a06312df4812'
